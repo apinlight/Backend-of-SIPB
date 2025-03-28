537ce92ffec4313388a78d9871e1e3d4d16bf5bb
apinlight <apin99lightning@gmail.com>
2025-03-28 14:17:19 +0700

    Adding and Refactoring Login Logic to API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+
+use App\Http\Controllers\Api\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Api\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Api\Auth\NewPasswordController;
+use App\Http\Controllers\Api\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Api\Auth\RegisteredUserController;
+use App\Http\Controllers\Api\Auth\VerifyEmailController;
+
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\BarangController;
 use App\Http\Controllers\Api\PengajuanController;
@@ -8,6 +17,37 @@ use App\Http\Controllers\Api\GudangController;
 use App\Http\Controllers\Api\BatasBarangController;
 use App\Http\Controllers\Api\BatasPengajuanController;
 use App\Http\Controllers\Api\DetailPengajuanController;
+
+
+// Auth Routes
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
 
 // Users Routes
 Route::apiResource('users', UserController::class);
@@ -18,7 +58,7 @@ Route::apiResource('barang', BarangController::class);
 // Pengajuan Routes
 Route::apiResource('pengajuan', PengajuanController::class);
 
-// DetailPengajuan routes
+// DetailPengajuan Routes
 Route::get('detail-pengajuan', [DetailPengajuanController::class, 'index']);
 Route::post('detail-pengajuan', [DetailPengajuanController::class, 'store']);
 Route::get('detail-pengajuan/{id_pengajuan}/{id_barang}', [DetailPengajuanController::class, 'show']);
@@ -26,7 +66,7 @@ Route::put('detail-pengajuan/{id_pengajuan}/{id_barang}', [DetailPengajuanContro
 Route::delete('detail-pengajuan/{id_pengajuan}/{id_barang}', [DetailPengajuanController::class, 'destroy']);
 
 
-// Gudang Routes - composite key requires custom parameter definitions:
+// Gudang Routes
 Route::get('gudang', [GudangController::class, 'index']);
 Route::post('gudang', [GudangController::class, 'store']);
 Route::get('gudang/{unique_id}/{id_barang}', [GudangController::class, 'show']);
